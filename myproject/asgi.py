235fc25e55137d2fd6aad38351f1e0304e442e01
joseph mathew <josephmathew18004@gmail.com>
2026-04-13 21:47:33 +0530

    added gunicorn

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_asgi_application()
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
