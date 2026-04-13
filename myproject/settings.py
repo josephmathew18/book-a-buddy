29edb97f7f7261c14c6be406dd18581ae0100c24
joseph mathew <josephmathew18004@gmail.com>
2026-04-13 17:24:24 +0530

    Fix Vercel deployment: Added requirements.txt, vercel.json, and ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d+t^0#p3bgwi^nt+@^s7+oa+-vqwh+78$i(tfnl7@eu&nzd2v-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
